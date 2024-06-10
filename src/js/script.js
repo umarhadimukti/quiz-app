6911d35775a85a263fdb4384300be387e4db88e0
umarhadimukti <umarhadimukti@gmail.com>
2024-06-10 19:27:01 +0700

    feat: 🎸 correct answer

--- src/js/script.js
+++ src/js/script.js
@@ -11,6 +11,9 @@ const buttonFinish = document.querySelector('.btn-finish');
 const inputs = document.querySelectorAll(".wrapper-answer input[type='radio']");
 const resultBox = document.querySelector('.result-box');
 const scoreElement = document.querySelector('span#score');
+const buttonAnswer = document.querySelector('.btn-answer');
+const answerBox = document.querySelector('.box-answer');
+const containerAnswer = document.querySelector('.box-answer .container-answer');
 
 
 buttonMain.addEventListener('click', function () {
@@ -21,7 +24,103 @@ buttonMain.addEventListener('click', function () {
 
 let currentQuestion = 0;
 let quizs = [];
+let userAnswers = [];
+let wrongAnswers = [];
 let result = 0;
+
+// reset inputs field
+const reset = () => {
+    inputs.forEach(input => {
+        input.checked = false;
+    })
+};
+
+// get answer value from user
+const getAnswerFromInput = () => {
+    let answer;
+    
+    inputs.forEach(input => {
+        if (input.checked) {
+            answer = input.value;
+            userAnswers.push(answer);
+        }
+    });
+
+    return answer;
+};
+
+// when user click correct answer button
+buttonAnswer.addEventListener('click', function () {
+    resultBox.style.display = 'none';
+    answerBox.style.display = 'block';
+    displayCorrectAnswer();
+});
+
+// when user click finish button
+buttonFinish.addEventListener('click', function () {
+    let answer = getAnswerFromInput();
+
+    if (answer === quizs[currentQuestion]['answer'].toUpperCase()) {
+        result++;
+    }
+
+    currentQuestion++;
+
+    if (getAnswerFromInput()) {
+        quizContainer.style.display = 'none';
+        resultBox.style.display = 'block';
+        scoreElement.innerText = `${result}/${quizs.length}`;
+    }
+});
+
+// when user click next button
+buttonNext.addEventListener('click', async function () {
+    await loadDataQuiz('../src/quiz.json');
+
+    let answer = getAnswerFromInput();
+
+    if (answer) {
+        if (answer === quizs[currentQuestion]['answer'].toUpperCase()) {
+            result++;
+        }
+
+        currentQuestion++;
+
+        if (currentQuestion < quizs.length) {
+            loadQuiz();
+        }
+    }
+});
+
+// displaying correct answers
+const displayCorrectAnswer = async () => {
+    await loadDataQuiz('../src/quiz.json');
+
+    quizs.forEach((quiz, i)=> {
+        const wrapperAnswerEl = document.createElement('div');
+        wrapperAnswerEl.classList.add('wrapper-answer');
+
+        const questionEl = document.createElement('p');
+        questionEl.textContent = `${i+1}.${quiz.question}`;
+        wrapperAnswerEl.appendChild(questionEl);
+
+        const userAnswerEl = document.createElement('p');
+        userAnswerEl.textContent = `Jawaban kamu: ${userAnswers[i].toUpperCase()}`;
+        userAnswerEl.style.color = 'black';
+        if (userAnswers[i] !== quiz['answer'].toUpperCase()) {
+            userAnswerEl.style.color = 'red';
+        }
+        wrapperAnswerEl.appendChild(userAnswerEl);
+
+        const correctAnswerEl = document.createElement('p');
+        correctAnswerEl.textContent = `Jawaban benar: ${quiz.answer.toUpperCase()}`;
+        correctAnswerEl.style.color = 'green';
+        wrapperAnswerEl.appendChild(correctAnswerEl);
+    
+        // insert wrapperAnswerEl into containerAnswer
+        containerAnswer.appendChild(wrapperAnswerEl);
+    });
+};
 
 // load data quiz from json
 const loadDataQuiz = async (url) => {
@@ -38,7 +137,7 @@ const loadDataQuiz = async (url) => {
         console.error('Gagal mendapatkan data:', err);
         throw err;
     }
-}
+};
 
 // load ui quiz
 const loadQuiz = async () => {
@@ -64,58 +163,6 @@ const loadQuiz = async () => {
         console.error(err);
     }
     
-}
-
-const reset = () => {
-    inputs.forEach(input => {
-        input.checked = false;
-    })
-}
-
-buttonFinish.addEventListener('click', function () {
-    let answer = getAnswerFromInput();
-
-    if (answer === quizs[currentQuestion]['answer'].toUpperCase()) {
-        result++;
-    }
-
-    currentQuestion++;
-
-    if (getAnswerFromInput()) {
-        quizContainer.style.display = 'none';
-        resultBox.style.display = 'block';
-        scoreElement.innerText = `${result}/${quizs.length}`;
-    }
-});
-
-buttonNext.addEventListener('click', async function () {
-    await loadDataQuiz('../src/quiz.json');
-
-    let answer = getAnswerFromInput();
-
-    if (answer) {
-        if (answer === quizs[currentQuestion]['answer'].toUpperCase()) {
-            result++;
-        }
-
-        currentQuestion++;
-
-        if (currentQuestion < quizs.length) {
-            loadQuiz();
-        }
-    }
-});
-
-const getAnswerFromInput = () => {
-    let answer;
-    
-    inputs.forEach(input => {
-        if (input.checked) {
-            answer = input.value;
-        }
-    });
-
-    return answer;
-}
+};
 
 loadQuiz();
